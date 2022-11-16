2048a75a4e71f1cd72c06f95b3869fa78dc9804b
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-11-16 20:19:43 +0300

    [Salih|Kaan|Omer] add register screen

--- lib/features/auth/screens/register_screen.dart
+++ lib/features/auth/screens/register_screen.dart
@@ -11,6 +11,7 @@ class RegisterScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         titleTextStyle: const TextStyle(color: Colors.black),
       ),
+      body: const RegisterWidget(),
     );
   }
 }
@@ -24,5 +25,227 @@ class RegisterWidget extends StatefulWidget {
 
 class _RegisterWidgetState extends State<RegisterWidget> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+    return Form(
+      key: formKey,
+      child: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Adınız',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Boş bırakmayınız';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Soy Adınız',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Boş bırakmayınız';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'E-Posta',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Boş bırakmayınız';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Şifreniz*',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Boş bırakmayınız';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'Şifre Tekrar*',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Boş bırakmayınız';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: const Color.fromARGB(255, 146, 127, 255),
+                        ),
+                        onPressed: () {
+                          // Validate will return true if the form is valid, or false if
+                          // the form is invalid.
+                          if (formKey.currentState!.validate()) {
+                            // Process data.
+                          }
+                        },
+                        child: const Text('Hesabımı Oluştur'),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: RichText(
+                      text: TextSpan(
+                        text: 'Lorem ipsum dolor sit amet,',
+                        style: DefaultTextStyle.of(context).style,
+                        children: const <TextSpan>[
+                          TextSpan(
+                              text: ' consectetur adipiscing elit,',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 146, 127, 255))),
+                          TextSpan(
+                              text:
+                                  '  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        height: 1.0,
+                        width: 60.0,
+                        color: const Color.fromARGB(255, 153, 153, 153),
+                      ),
+                    ),
+                    const Text(
+                      'Yada',
+                      style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color.fromARGB(255, 153, 153, 153)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Container(
+                        height: 1.0,
+                        width: 60.0,
+                        color: const Color.fromARGB(255, 153, 153, 153),
+                      ),
+                    ),
+                  ],
+                ),
+                Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        TextButton(
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.all<Color>(
+                              const Color.fromARGB(255, 146, 127, 255),
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset('assets/images/google.png',
+                                  fit: BoxFit.contain,
+                                  width: 24.0,
+                                  height: 24.0),
+                              const Text(
+                                'Google ile kayıt ol',
+                                style: TextStyle(
+                                    fontSize: 15.0, color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Hesabınız var mı?',
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                    GestureDetector(
+                      onTap: () {},
+                      child: const Text(
+                        'Giriş Yapın',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Color.fromARGB(255, 146, 127, 255),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20.0),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
