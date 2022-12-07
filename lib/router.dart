6381cc571bd0ab63b0ab227ae15ea49d09530b83
M.Salih Gumus <gumusmuhammedsalih@gmail.com>
2022-12-07 20:49:55 +0300

    [Salih]  fix routing

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:pickapp/app/modules/home/home_screen.dart';
 import 'package:pickapp/app/modules/login/login_screen.dart';
 import 'package:pickapp/app/modules/register/register_screen.dart';
@@ -6,13 +7,25 @@ import 'package:pickapp/app/modules/welcome/welcome_screen.dart';
 
 import 'package:routemaster/routemaster.dart';
 
-//If we have splash screen make '/' to spash screen
-final loggedOutRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: WelcomeScreen()),
+final box = GetStorage();
+final routes = RouteMap(routes: {
+  '/': (_) => canUserAccessPage()
+      ? const MaterialPage(child: HomeScreen())
+      : const MaterialPage(child: WelcomeScreen()),
   '/login': (_) => const MaterialPage(child: LoginScreen()),
   '/register': (_) => const MaterialPage(child: RegisterScreen()),
+  '/home': (_) => canUserAccessPage()
+      ? const MaterialPage(child: HomeScreen())
+      : const Redirect('/welcome'),
 });
 
-final loggedInRoute = RouteMap(routes: {
-  '/': (_) => const MaterialPage(child: HomeScreen()),
-});
+canUserAccessPage() {
+  bool canAccess = false;
+  var accessToken = box.read("accessToken");
+  debugPrint('abbas: $accessToken');
+
+  if (accessToken != null && accessToken != "") {
+    canAccess = true;
+  }
+  return canAccess;
+}
